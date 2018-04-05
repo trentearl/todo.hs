0e91bb8fdebcde76ea2eb0a3a184e3f28ffa4c99
trent.earl <trent.earl@malauzai.com>
2018-04-05 16:46:22 +0300

    Add auto refresh

--- src/view/inner.jsx
+++ src/view/inner.jsx
@@ -1,9 +1,37 @@
+import DB from 'pouchdb';
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
+import { connect } from 'react-redux';
 
+import { taskReorder, taskNew, tasksRefresh } from './actions/index';
 import Tasks from './tasks';
 
 class Inner extends Component {
+    componentDidMount() {
+        var sync = DB.sync('https://couch.trentearl.com/todo', 'todo', {
+            live: true,
+            username: 'admin',
+            password: 'we are trent',
+            retry: true
+        })
+        .on('change', _ => this.props.dispatch(tasksRefresh()))
+        .on('paused', function (err) {
+            console.log('paused ', err);
+        })
+        .on('active', function () {
+            console.log('active');
+        })
+        .on('denied', function (err) {
+            console.log('denied', err);
+        })
+        .on('complete', function (info) {
+            console.log('compete', info);
+        })
+        .on('error', function (err) {
+            console.log('error', err);
+        });
+    }
+
     render() {
         return (
             <div>
@@ -13,5 +41,5 @@ class Inner extends Component {
     }
 }
 
-export default Inner;
+export default connect()(Inner);
 
